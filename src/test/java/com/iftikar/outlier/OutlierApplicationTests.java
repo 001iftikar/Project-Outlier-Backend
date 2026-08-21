bde763d5b6a216c86b5f76ec0b46ca24e114826e
Iftikar Hussain <iftikarraza8@gmail.com>
2026-08-21 19:25:21 +0530

    Initial commit

---
+++ src/test/java/com/iftikar/outlier/OutlierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iftikar.outlier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutlierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
